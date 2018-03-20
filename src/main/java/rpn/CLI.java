660074c2c35db411c6e40a988d1fdd128825a6f9
mickafon <micaelafonso@free.fr>
2018-03-20 14:52:08 +0100

    correction

--- src/main/java/rpn/CLI.java
+++ src/main/java/rpn/CLI.java
@@ -8,8 +8,7 @@ import java.util.stream.Stream;
 
 public class CLI {
     public static final void main(String[] args) {
-        //String expression = Stream.of(args).collect(Collectors.joining(" "));
-        String expression = "2 3 5 + +";
+        String expression = Stream.of(args).collect(Collectors.joining(" "));
         System.out.println("About to evaluate '" + expression + "'");
         double result = evaluate(expression);
         System.out.println("> " + result);
