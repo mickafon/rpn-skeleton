9e5e94651e5bb3e97318fb26941ed3a7e7774424
CXIR <cxir.developer@gmail.com>
2018-03-20 22:45:11 +0100

    Commit March 20
    Clean Code + Tests

--- src/main/java/rpn/CLI.java
+++ src/main/java/rpn/CLI.java
@@ -1,14 +1,13 @@
 package rpn;
 
-import java.math.BigDecimal;
-import java.math.RoundingMode;
-import java.util.Collections;
 import java.util.Stack;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class CLI {
+
     public static final void main(String[] args) {
+
         String expression = Stream.of(args).collect(Collectors.joining(" "));
         System.out.println("About to evaluate '" + expression + "'");
         String result = evaluate(expression);
@@ -16,93 +15,122 @@ public class CLI {
     }
 
     static String evaluate(String expression) {
-        if (expression == null || expression == ""){
+
+        if (    expression == null
+             || expression == "" ){
+
             return "";
         }
 
         Stack<String> stack = new Stack();
 
-        String tmp = "";
+        String tmp   = "";
         int posPoint = 0;
-        if (isNumeric(expression)){
+
+        if( isNumeric( expression ) ) {
+
             return expression;
         }
-        for(int i = 0; i < expression.length(); i++){
+
+        for( int i = 0; i < expression.length(); i++ ) {
+
             char carac = expression.charAt(i);
-            if (carac == '.' || carac == ','){
+
+            if( carac == '.' || carac == ',' ) {
+
                 posPoint = i;
                 continue;
             }
-            if(carac == ' '){
-                if(tmp != ""){
+            if( carac == ' ' ) {
+
+                if( tmp != "" ) {
+
                     stack.push(tmp);
                 }
-                tmp = "";
+                tmp      = "";
                 posPoint = 0;
             }
             else{
-                if(stack.size() > 1){
-                    if(carac == '+' || carac == '-' || carac == '*' || carac == '/'){
+                if( stack.size() > 1 ) {
+
+                    if( carac == '+'
+                        || carac == '-'
+                        || carac == '*'
+                        || carac == '/' ) {
+
                         tmp = stack.peek();
                         stack.pop();
-                        tmp = calculate(stack.peek(), tmp, carac);
+
+                        tmp = calculate( stack.peek(), tmp, carac );
+
                         stack.pop();
                         stack.push(tmp);
                         tmp = "";
+
                         continue;
                     }
                 }
-                if (tmp.equals("")){
+                if( tmp.equals("") ) {
+
                     tmp = "0";
                 }
-                if (posPoint > 0){
-                    double value = Double.parseDouble(tmp) + Character.getNumericValue(carac) * Math.pow(10, (i - posPoint) * -1);
+                if( posPoint > 0 ) {
+
+                    double value = Double.parseDouble( tmp ) + Character.getNumericValue( carac ) * Math.pow( 10, ( i - posPoint ) * -1);
                     tmp = String.valueOf(value);
                 }
                 else {
-                    double value = Double.parseDouble(tmp) * Math.pow(10, i) + Character.getNumericValue(carac);
-                    tmp = String.valueOf(value);
+
+                    double value = Double.parseDouble( tmp ) * Math.pow( 10, i ) + Character.getNumericValue( carac );
+                    tmp = String.valueOf( value );
                 }
             }
         }
-        if (stack.size() == 1){
+        if( stack.size() == 1 ) {
+
             return stack.peek();
         }
-        stack.push(tmp);
+
+        stack.push( tmp );
         StringBuffer buf = new StringBuffer();
-        for (int i = 0; i < stack.size(); i++) {
-            buf.append(stack.elementAt(i));
+
+        for( int i = 0; i < stack.size(); i++ ) {
+
+            buf.append( stack.elementAt( i ) );
             buf.append(" ");
         }
 
         return buf.toString();
     }
 
-    static String calculate(String str1, String str2, char operation){
-        double nb1 = Double.parseDouble(str1);
-        double nb2 = Double.parseDouble(str2);
-        switch(operation){
+    static String calculate( String str1, String str2, char operation ) {
+
+        double nb1 = Double.parseDouble( str1 );
+        double nb2 = Double.parseDouble( str2 );
+
+        switch( operation ) {
             case '+':
-                return String.valueOf(nb1 + nb2);
+                return String.valueOf( nb1 + nb2 );
             case '-':
-                return String.valueOf(nb1 - nb2);
+                return String.valueOf( nb1 - nb2 );
             case '*':
-                return String.valueOf(nb1 * nb2);
+                return String.valueOf( nb1 * nb2 );
             case '/':
-                if (nb2 == 0){
-                    throw new ArithmeticException("Cannot divise to 0");
+                if( nb2 == 0 ) {
+
+                    throw new ArithmeticException( "Cannot divise to 0!!" );
                 }
-                return String.valueOf(nb1 / nb2);
+                return String.valueOf( nb1 / nb2 );
         }
-        return String.valueOf(0);
+        return String.valueOf( 0 );
     }
 
-    private static boolean isNumeric(String expression){
+    private static boolean isNumeric( String expression ) {
         try
         {
-            double num = Double.parseDouble(expression);
+            double num = Double.parseDouble( expression );
         }
-        catch(NumberFormatException nfe)
+        catch( NumberFormatException nfe )
         {
             return false;
         }
--- src/test/java/rpn/CLITest.java
+++ src/test/java/rpn/CLITest.java
@@ -9,21 +9,78 @@ public class CLITest {
 
     @Test
     public void should_evaluate_single_digit_constant() {
-        assertThat(evaluate("5")).isEqualTo(5);
+        String expected = String.valueOf(5);
+        assertThat(evaluate("5")).isEqualTo(expected);
     }
 
     @Test
     public void should_evaluate_multiple_digits_constant() {
-        assertThat(evaluate("17")).isEqualTo(17);
+        String expected = String.valueOf(17);
+        assertThat(evaluate("17")).isEqualTo(expected);
     }
 
     @Test
     public void should_evaluate_simple_addition() {
-        assertThat(evaluate("17 5 +")).isEqualTo(22);
+        String expected = String.valueOf(22.0);
+        assertThat(evaluate("17 5 +")).isEqualTo(expected);
     }
 
     @Test
     public void should_evaluate_more_complex_addition() {
-        assertThat(evaluate("2 3 5 + +")).isEqualTo(10);
+        String expected = String.valueOf(10.0);
+        assertThat(evaluate("2 3 5 + +")).isEqualTo(expected);
+    }
+
+    @Test
+    public void should_evaluate_simple_soustraction() {
+        String expected = String.valueOf(12.0);
+        assertThat(evaluate("17 5 -")).isEqualTo(expected);
+    }
+
+    @Test
+    public void should_evaluate_more_complex_soustraction() {
+        String expected = String.valueOf(1.0);
+        assertThat(evaluate("10 2 7 + -")).isEqualTo(expected);
+    }
+
+    @Test
+    public void should_evaluate_simple_multiplication() {
+        String expected = String.valueOf(85.0);
+        assertThat(evaluate("17 5 *")).isEqualTo(expected);
+    }
+
+    @Test
+    public void should_evaluate_more_complex_multiplication() {
+        String expected = String.valueOf(140.0);
+        assertThat(evaluate("10 2 7 * *")).isEqualTo(expected);
+    }
+
+    @Test
+    public void should_evaluate_simple_division() {
+        String expected = String.valueOf(200.0);
+        assertThat(evaluate("100 5 /")).isEqualTo(expected);
+    }
+
+    @Test
+    public void should_evaluate_more_complex_division() {
+        String expected = String.valueOf(25.0);
+        assertThat(evaluate("10 2 5 / /")).isEqualTo(expected);
+    }
+
+    @Test(expected = ArithmeticException.class)
+    public void should_not_evaluate_division_by_zero() {
+        evaluate("4 0 /");
+    }
+
+
+    @Test
+    public void should_evaluate_operation_with_decimal(){
+        String expected = String.valueOf(4.0);
+        assertThat(evaluate("2.1 1.9 +")).isEqualTo(expected);
+    }
+
+    @Test
+    public void should_concat_numbers_when_no_operande(){
+        assertThat(evaluate("7 2 - 3 4")).isEqualTo("5.0 3.0 4.0 ");
     }
 }
